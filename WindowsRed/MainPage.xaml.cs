f0b50a6a99c0c48a10f2d00ed84a9f7f9795ba9e
Kaniel Yano <kaniel.yano@gmail.com>
2012-11-14 23:17:21 -0800

    Initial Windows Store App commit.
    Quick LinksPage UI that displays the frontpage of Reddit.

---
+++ WindowsRed/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="non.io">
+// © non.io
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace WindowsRed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+    using WindowsRed.Views;
+
+    /// <summary>
+    /// MainPage class.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage" /> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += this.FrameLoaded;
+        }
+
+        /// <summary>
+        /// Frames the loaded.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs" /> instance containing the event data.</param>
+        private void FrameLoaded(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(LinksPage));
+        }
+    }
+}
---
+++ WindowsRed/Views/LinksPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="LinksPage.xaml.cs" company="non.io">
+// © non.io
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace WindowsRed.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Linq;
+    using RedditApi8;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Primitives;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Input;
+    using Windows.UI.Xaml.Media;
+    using Windows.UI.Xaml.Navigation;
+
+    /// <summary>
+    /// LinksPage class.
+    /// </summary>
+    public sealed partial class LinksPage : Page
+    {
+        /// <summary>
+        /// The client.
+        /// </summary>
+        private RedditClient client;
+
+        /// <summary>
+        /// The links.
+        /// </summary>
+        private ObservableCollection<LinkData> links;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LinksPage" /> class.
+        /// </summary>
+        public LinksPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.client = new RedditClient("WindowsRed");
+            this.links = new ObservableCollection<LinkData>();
+            foreach (LinkData link in await this.client.GetFrontPageAsync())
+            {
+                this.links.Add(link);
+            }
+
+            this.mainList.ItemsSource = this.links;
+        }
+    }
+}
